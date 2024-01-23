0196d31d584328a920839b4c93346f6a1408a0a3
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-01-23 22:24:01 +0200

    Update tabele script

--- section_01/tabele.sql
+++ section_01/tabele.sql
@@ -1,13 +1,16 @@
-CREATE TABLE regiuni {
+CREATE TABLE regiuni (
     regiune_id int(11) PRIMARY KEY AUTO_INCREMENT,
     regiune_nume varchar(255) NOT NULL,
     regiune_stare boolean NOT NULL DEFAULT 0
-}
+)
 
 
-CREATE TABLE judete {
+CREATE TABLE judete (
     judet_id int(11) PRIMARY KEY AUTO_INCREMENT,
     judet_nume varchar(255) NOT NULL,
     judet_regiune int(11) NOT NULL
     judet_stare boolean NOT NULL DEFAULT 0
-}
+)
+
+DROP TABLE IF EXISTS regiuni;
+DROP TABLE IF EXISTS judete;
